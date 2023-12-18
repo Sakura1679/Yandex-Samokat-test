24dcafd3490b77771a95015378a9f426bd23470e
Sakura1679 <149877450+Sakura1679@users.noreply.github.com>
2023-12-18 22:02:47 +0300

    Update get_order_test.py

--- get_order_test.py
+++ get_order_test.py
@@ -1,6 +1,5 @@
-
 #Александр Куринец, 11 когорта, финальный проект, Инженер по тестированию плюс.
-#Не понимаю, почему не получается нужный результат, через Postman возвращается код 200
+
 
 import sender_stand_request
 import data
@@ -10,4 +9,8 @@ def test_get_order_by_track_success_response():
     track_number = create_order_response.json()['track']
     get_order_response = sender_stand_request.get_order(track_number)
     assert get_order_response.status_code == 200
+    if get_order_response.status_code == 200:
+        print("Тест пройден")
+    else:
+        print("Тест провален")
 
